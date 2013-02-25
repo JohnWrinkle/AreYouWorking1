8be806f39630b83c074ffecc4a734656b7c27b73
Jared Corso <jaredcorso@gmail.com>
2013-02-25 18:38:20 -0500

    Put button onClick actions into seperate functions

--- src/com/johnwrinkle/areyouworking1/MainActivity.java
+++ src/com/johnwrinkle/areyouworking1/MainActivity.java
@@ -2,25 +2,22 @@ package com.johnwrinkle.areyouworking1;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.provider.AlarmClock;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	private Button mYButton;
-	private Button mNButton;
-	private Button mCoolDownButton;
+	//private Button mYButton;
+	//private Button mNButton;
+	//private Button mCoolDownButton;
 	private EditText mCoolDownNum;
 	private TextView mMainText;
 
@@ -43,139 +40,129 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		mYButton = (Button) findViewById(R.id.yes);
-		mNButton = (Button) findViewById(R.id.no);
-		mCoolDownButton = (Button) findViewById(R.id.coolDownButton);
+		//mYButton = (Button) findViewById(R.id.yes);
+		//mNButton = (Button) findViewById(R.id.no);
+		//mCoolDownButton = (Button) findViewById(R.id.coolDownButton);
 		mCoolDownNum = (EditText) findViewById(R.id.coolDownNum);
 		mMainText = (TextView) findViewById(R.id.mainText);
+		
+	}
+	
+	public void sendYes(View view){
+		working = true;
+		final Ringtone r = RingtoneManager.getRingtone(
+		getApplicationContext(), notification);
+		
+		// this message goes away right away and changes to seconds
+		// remaining
+		// i could ignore this problem and just do Great job: seconds
+		// remain
+		// or i could figure out how to make it wait ~5 seconds then go
+		// onto the next message...
+		// something to do with ontick idk not gonna mess with it now
 
-		mCoolDownButton.setOnClickListener(new View.OnClickListener() {
+		//why is this being set to just be overwritten
+		//mMainText.setText("Great job!");
+	
 
-			@Override
-			public void onClick(View v) {
+		new CountDownTimer(goalCoolDownTimer * 1000 *60, 1000) {
 
-				// First we caste mCoolDownNum(user input) to a string
-				String casteCoolDownNum = mCoolDownNum.getText().toString();
-				// Then we caste that result (casteCoolDownNum) into an int
-				int intCoolDownNum = Integer.parseInt(casteCoolDownNum);
-				// then we will make sure the number is greater than 0
-				if (intCoolDownNum > 0) {
-					// if the number is greater than 0 we will set it as our
-					// goalCoolDownTimer
-					goalCoolDownTimer = intCoolDownNum;
-					// Give them a toast that it was successful
-					Context context = getApplicationContext();
-					CharSequence text = "Working cool down set to "
-							+ intCoolDownNum + " seconds";
-					int duration = Toast.LENGTH_SHORT;
+			public void onTick(long millisUntilFinished) {
+				mMainText.setText("seconds remaining: "
+						+ millisUntilFinished / 1000);
+			}
 
-					Toast toast = Toast.makeText(context, text, duration);
-					toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
-					toast.show();
-
-				}
-
+			public void onFinish() {
+				mMainText.setText("done!");
+				r.play();
 				/*
-				 * 
-				 * need to figure out the proper way to say this.
-				 * 
-				 * 
-				 * if(goalCoolDownTimer >0) { goalCoolDownTimer = value of the
-				 * mcoolDownNum; }
-				 */
+				Alarm does not work
+				Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
+				i.putExtra(AlarmClock.EXTRA_HOUR, 0);
+				i.putExtra(AlarmClock.EXTRA_MINUTES, goalCoolDownTimer);
+				startActivity(i);
+				http://stackoverflow.com/questions/4989757/how-to-set-alarm-using-alarm-clock-class
+				*/
+				// this is super sloppy i feel like there is a better
+				// way with a while loop?
+				mMainText.setText("Are you working?");
 
 			}
-		});
-		mYButton.setOnClickListener(new View.OnClickListener() {
+		}.start();
+		// right now this does nothing!
+		// it wont loop
+		// maybe make the loop into some kind of class?
+		// yea a class i can call onto to start
 
-			@Override
-			public void onClick(View v) {
-				working = true;
-				final Ringtone r = RingtoneManager.getRingtone(
-				getApplicationContext(), notification);
-				
-				// this message goes away right away and changes to seconds
-				// remaining
-				// i could ignore this problem and just do Great job: seconds
-				// remain
-				// or i could figure out how to make it wait ~5 seconds then go
-				// onto the next message...
-				// something to do with ontick idk not gonna mess with it now
-
-				mMainText.setText("Great job!");
-
-				new CountDownTimer(goalCoolDownTimer * 1000 *60, 1000) {
-
-					public void onTick(long millisUntilFinished) {
-						mMainText.setText("seconds remaining: "
-								+ millisUntilFinished / 1000);
-					}
-
-					public void onFinish() {
-						mMainText.setText("done!");
-						r.play();
-						/*
-						Alarm does not work
-						Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
-						i.putExtra(AlarmClock.EXTRA_HOUR, 0);
-						i.putExtra(AlarmClock.EXTRA_MINUTES, goalCoolDownTimer);
-						startActivity(i);
-						http://stackoverflow.com/questions/4989757/how-to-set-alarm-using-alarm-clock-class
-						*/
-						// this is super sloppy i feel like there is a better
-						// way with a while loop?
-						mMainText.setText("Are you working?");
-
-					}
-				}.start();
-				// right now this does nothing!
-				// it wont loop
-				// maybe make the loop into some kind of class?
-				// yea a class i can call onto to start
-
-				// working = false;
-
-			}
-		});
-
-		mNButton.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				working = true;
-				final Ringtone r = RingtoneManager.getRingtone(
-						getApplicationContext(), notification);
-				mMainText.setText("You really should be working right now!");
-				new CountDownTimer(punishmentTimer * 1000, 1000) {
-
-					public void onTick(long millisUntilFinished) {
-						mMainText.setText("seconds remaining: "
-								+ millisUntilFinished / 1000);
-					}
-
-					public void onFinish() {
-						mMainText.setText("done!");
-						r.play();
-						// this is super sloppy i feel like there is a better
-						// way with a while loop?
-						mMainText.setText("Are you working?");
-
-					}
-				}.start();
-				// working = false;
-
-			}
-		});
-
-		/*
-		 * while (working = false) { // do stuff
-		 * mMainText.setText("Are you working right now?");
-		 * 
-		 * 
-		 * }
-		 */
+		// working = false;
 
 	}
+	
+	public void sendNo(View view){
+		working = true;
+		final Ringtone r = RingtoneManager.getRingtone(
+				getApplicationContext(), notification);
+		mMainText.setText("You really should be working right now!");
+		new CountDownTimer(punishmentTimer * 1000, 1000) {
+
+			public void onTick(long millisUntilFinished) {
+				mMainText.setText("seconds remaining: "
+						+ millisUntilFinished / 1000);
+			}
+
+			public void onFinish() {
+				mMainText.setText("done!");
+				r.play();
+				// this is super sloppy i feel like there is a better
+				// way with a while loop?
+				mMainText.setText("Are you working?");
+
+			}
+		}.start();
+		// working = false;
+
+	}
+	
+	/**
+	 * while (working = false) { // do stuff
+	 * mMainText.setText("Are you working right now?");
+	 * 
+	 * 
+	 * }
+	 */
+	
+	public void sendCoolDown(View view){
+		// First we caste mCoolDownNum(user input) to a string
+		String casteCoolDownNum = mCoolDownNum.getText().toString();
+		// Then we caste that result (casteCoolDownNum) into an int
+		int intCoolDownNum = Integer.parseInt(casteCoolDownNum);
+		// then we will make sure the number is greater than 0
+		if (intCoolDownNum > 0) {
+			// if the number is greater than 0 we will set it as our
+			// goalCoolDownTimer
+			goalCoolDownTimer = intCoolDownNum;
+			// Give them a toast that it was successful
+			Context context = getApplicationContext();
+			CharSequence text = "Working cool down set to "
+					+ intCoolDownNum + " seconds";
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+			toast.show();
+
+		}
+
+		/*
+		 * 
+		 * need to figure out the proper way to say this.
+		 * 
+		 * 
+		 * if(goalCoolDownTimer >0) { goalCoolDownTimer = value of the
+		 * mcoolDownNum; }
+		 */
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
